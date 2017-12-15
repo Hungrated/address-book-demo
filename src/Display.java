e16cb1066acd5530bf3a869ab56d83e2c9e98200
hungrated <zhang295415658@qq.com>
2017-12-16 00:46:46 +0800

    modify layout

--- src/Display.java
+++ src/Display.java
@@ -31,6 +31,7 @@ public class Display extends JFrame {
 
         // set bounds
         this.setBounds(100, 100, 500, 600);
+        this.setResizable(false);
 
         // set layout
         this.setLayout(new FlowLayout());
@@ -44,7 +45,7 @@ public class Display extends JFrame {
     private void addLeftPanel() {
         // left panel
         JPanel options = new JPanel();
-        options.setBorder(new EmptyBorder(5, 5, 5, 5));
+        options.setBorder(new EmptyBorder(10, 5, 5, 5));
         options.setLayout(new FlowLayout());
         this.add(options);
 
@@ -54,12 +55,17 @@ public class Display extends JFrame {
         options.add(insert);
         options.add(modify);
         options.add(query);
+
+        // insert event listener
+        insert.addActionListener(e -> {
+            this.setInsertDataWindow();
+        });
+
     }
 
     private void addRightPanel() {
         //right panel
         JPanel queryDisplay = new JPanel();
-        queryDisplay.setBorder(new EmptyBorder(5, 5, 5, 5));
         queryDisplay.setLayout(new FlowLayout());
         this.add(queryDisplay);
 
@@ -68,7 +74,46 @@ public class Display extends JFrame {
         textArea.setRows(15);
         textArea.setEditable(false);
         queryDisplay.add(textArea);
+    }
 
+    private void setInsertDataWindow() {
+        JFrame insertData = new JFrame("添加联系人");
+
+        // set bounds
+        insertData.setBounds(150, 150, 300, 220);
+        this.setResizable(false);
+
+        // set layout
+        insertData.setLayout(new FlowLayout());
+
+        // set input
+        JTextField name = new JTextField(20);
+        JTextField phone = new JTextField(20);
+        JTextField addr = new JTextField(20);
+
+        JPanel namePanel = new JPanel();
+        JPanel phonePanel = new JPanel();
+        JPanel addrPanel = new JPanel();
+        this.setInput(insertData, namePanel, "姓 名", name);
+        this.setInput(insertData, phonePanel, "电 话", phone);
+        this.setInput(insertData, addrPanel, "地 址", addr);
+
+        // set button
+        Button okBtn = new Button("确 定");
+        Button cancelBtn = new Button("取 消");
+
+        insertData.add(okBtn);
+        insertData.add(cancelBtn);
+
+        insertData.setVisible(true);
+    }
+
+    private void setInput(JFrame superFrame, JPanel panel, String label, JTextField input) {
+        panel.setLayout(new FlowLayout());
+        JLabel key = new JLabel(label);
+        panel.add(key);
+        panel.add(input);
+        superFrame.add(panel);
     }
 
 
